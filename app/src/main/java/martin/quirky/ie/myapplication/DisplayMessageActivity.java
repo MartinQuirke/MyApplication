f0c4676688444942eb5fe157a39b0bd5270de3f9
Martin Quirke <quirke88@gmail.com>
2014-11-13 22:57:27 +0000

    Added Intent, Shows message enter on second activity. Action bar re-added to second activity.

--- app/src/main/java/martin/quirky/ie/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/martin/quirky/ie/myapplication/DisplayMessageActivity.java
@@ -1,10 +1,12 @@
 package martin.quirky.ie.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends Activity {
@@ -13,6 +15,20 @@ public class DisplayMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        // If your minSdkVersion is 11 or higher, instead use:
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
     @Override
